5a4699478271d0731fe5ecf71ed7bea0fb4e95bd
Karol Torun <krol22ee@gmail.com>
2021-06-02 17:27:34 +0200

    empty folders, scripts, fonts

--- src/styled/Fonts.js
+++ src/styled/Fonts.js
@@ -1,6 +1,17 @@
 import {createGlobalStyle} from "styled-components";
 
-/* Import and declare all of your @font-faces rules */
+/*
+  Import and declare all of your @font-faces rules
+  f.e: 
+
+  @font-face {
+    font-family: "";
+    src: url() format("");
+    font-weight: --- ;
+    font-style: normal/italic;
+  }
+*/
+
 const Fonts = createGlobalStyle`
 
 `;
